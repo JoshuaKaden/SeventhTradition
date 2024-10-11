dec84fd34f109de443f8cdeb83eb5c9d57de0e93
Joshua Kaden <jkaden@mac.com>
2024-10-11 14:52:53 -0400

    ok we can edit the name

--- SeventhTradition/MeetingView.swift
+++ SeventhTradition/MeetingView.swift
@@ -11,19 +11,72 @@ struct MeetingView: View {
     
     @Binding var meeting: Meeting?
     
+    @Environment(\.dismiss) private var dismiss
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var isEditing: Bool = false
+    @State private var name: String = ""
+    
     var body: some View {
-        VStack {
-            if let meeting {
-                Text("Name")
-                    .font(.footnote)
-                Text(meeting.name)
-                    .padding(.bottom)
-                
-                Text("Meeting ID")
-                    .font(.footnote)
-                Text(meeting.id.uuidString)
-                    .font(.footnote)                
+        if let meeting {
+            Form {
+                if isEditing == true {
+                    Section("Name") {
+                        TextField(text: $name, label: {})
+                    }
+                } else {
+                    Section {
+                        VStack(alignment: .leading) {
+                            Text("Name")
+                                .font(.footnote)
+                            Text(meeting.name)
+                                .padding(.bottom)
+                        }
+                        VStack(alignment: .leading) {
+                            Text("Meeting ID")
+                                .font(.footnote)
+                            Text(meeting.id.uuidString)
+                                .font(.footnote)
+                        }
+                    }
+                }
             }
+            .task {
+                name = meeting.name
+            }
+            .animation(nil, value: isEditing)
+            .toolbar {
+                if isEditing == true {
+                    ToolbarItem {
+                        Button(action: saveMeeting) {
+                            Label("Save", systemImage: "square.and.arrow.down")
+                        }
+                    }
+                } else {
+                    ToolbarItem {
+                        Button(action: toggleIsEditing) {
+                            Label("Edit", systemImage: "square.and.pencil")
+                        }
+                    }
+                }
+            }
+        } else {
+            ContentUnavailableView("No meeting selected", systemImage: "bubble.left.and.exclamationmark.bubble.right")
+        }
+    }
+    
+    private func saveMeeting() {
+        if let meeting {
+            withAnimation {
+                meeting.name = name
+                isEditing = false
+            }
+        }
+    }
+    
+    private func toggleIsEditing() {
+        withAnimation {
+            isEditing.toggle()
         }
     }
 }
