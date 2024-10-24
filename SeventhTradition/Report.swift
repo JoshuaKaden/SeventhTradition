7096f940906ef0fbcdcfd54acb9535bef34f14fa
Joshua Kaden <jkaden@mac.com>
2024-10-24 15:06:05 -0400

    chef's kiss emoji

--- SeventhTradition/Report.swift
+++ SeventhTradition/Report.swift
@@ -13,11 +13,11 @@ struct Report: View {
     @Environment(\.currencyCode) private var currencyCode
     @Environment(\.meeting) private var meeting
 
-    @Query(sort: \Collection.date, order: .reverse) private var collections: [Collection]
-    @Query(sort: \GroupConsciencePayment.date, order: .reverse) private var groupConsciencePayments: [GroupConsciencePayment]
-    @Query(sort: \OtherExpense.date, order: .reverse) private var otherExpenses: [OtherExpense]
-    @Query(sort: \OtherIncome.date, order: .reverse) private var otherIncomes: [OtherIncome]
-    @Query(sort: \RentPayment.date, order: .reverse) private var rentPayments: [RentPayment]
+    @Query(sort: \Collection.date) private var collectionsQuery: [Collection]
+    @Query(sort: \GroupConsciencePayment.date) private var groupConsciencePaymentsQuery: [GroupConsciencePayment]
+    @Query(sort: \OtherExpense.date) private var otherExpensesQuery: [OtherExpense]
+    @Query(sort: \OtherIncome.date) private var otherIncomesQuery: [OtherIncome]
+    @Query(sort: \RentPayment.date) private var rentPaymentsQuery: [RentPayment]
     
     @State private var startDate: Date = Date().startDateOfMonth
     @State private var endDate: Date = Date().endDateOfMonth
@@ -26,27 +26,27 @@ struct Report: View {
         
         if let meeting {
 
-            let collectionsTotalBB = collections
+            let collectionsTotalBB = collectionsQuery
                 .filter({ $0.meeting == meeting && $0.date < startDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
-            let groupConscienceTotalBB = groupConsciencePayments
+            let groupConscienceTotalBB = groupConsciencePaymentsQuery
                 .filter({ $0.meeting == meeting && $0.date < startDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
-            let otherExpensesTotalBB = otherExpenses
+            let otherExpensesTotalBB = otherExpensesQuery
                 .filter({ $0.meeting == meeting && $0.date < startDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
-            let otherIncomeTotalBB = otherIncomes
+            let otherIncomeTotalBB = otherIncomesQuery
                 .filter({ $0.meeting == meeting && $0.date < startDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
-            let rentPaymentsTotalBB = rentPayments
+            let rentPaymentsTotalBB = rentPaymentsQuery
                 .filter({ $0.meeting == meeting && $0.date < startDate })
                 .map { $0.amount }
                 .reduce(0, +)
@@ -55,28 +55,33 @@ struct Report: View {
             let spentBB = rentPaymentsTotalBB + groupConscienceTotalBB + otherExpensesTotalBB
             let startingBalance = meeting.beginningBalance + gotBB - spentBB
             
+            let collections = collectionsQuery
+                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
             let collectionsTotal = collections
-                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
+            let groupConsciencePayments = groupConsciencePaymentsQuery
+                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
             let groupConscienceTotal = groupConsciencePayments
-                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
+            let otherExpenses = otherExpensesQuery
+                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
             let otherExpensesTotal = otherExpenses
-                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
+            let otherIncomes = otherIncomesQuery
+                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
             let otherIncomeTotal = otherIncomes
-                .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
                 .map { $0.amount }
                 .reduce(0, +)
             
-            let rentPaymentsTotal = rentPayments
+            let rentPayments = rentPaymentsQuery
                 .filter({ $0.meeting == meeting && $0.date >= startDate && $0.date <= endDate })
+            let rentPaymentsTotal = rentPayments
                 .map { $0.amount }
                 .reduce(0, +)
 
@@ -85,58 +90,128 @@ struct Report: View {
             let endingBalance = startingBalance + got - spent - meeting.prudentReserve
             
             List {
-                DatePicker("Start date", selection: $startDate)
-                DatePicker("End date", selection: $endDate)
+                Section {
+                    DatePicker("Start date", selection: $startDate)
+                    DatePicker("End date", selection: $endDate)
+                    
+                    HStack {
+                        Text("Starting Balance")
+                            .bold()
+                        Spacer()
+                        Text(startingBalance.formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                            .bold()
+                    }
+                    HStack {
+                        Text("Collections")
+                        Spacer()
+                        Text(collectionsTotal.formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                    }
+                    HStack {
+                        Text("Other Income")
+                        Spacer()
+                        Text(otherIncomeTotal.formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                    }
+                    HStack {
+                        Text("Rent")
+                        Spacer()
+                        Text((rentPaymentsTotal * -1).formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                    }
+                    HStack {
+                        Text("Other Expenses")
+                        Spacer()
+                        Text((otherExpensesTotal * -1).formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                    }
+                    HStack {
+                        Text("Prudent Reserve")
+                        Spacer()
+                        Text((meeting.prudentReserve * -1).formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                    }
+                    HStack {
+                        Text("Group Conscience")
+                        Spacer()
+                        Text((groupConscienceTotal * -1).formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                    }
+                    HStack {
+                        Text("Ending Balance")
+                            .bold()
+                        Spacer()
+                        Text(endingBalance.formatted(.currency(code: currencyCode)))
+                            .monospaced()
+                            .bold()
+                    }
+                }
                 
-                HStack {
-                    Text("Starting Balance")
-                    Spacer()
-                    Text(startingBalance.formatted(.currency(code: currencyCode)))
-                        .monospaced()
+                Section("Collections") {
+                    ForEach(collections) { collection in
+                        HStack {
+                            Text(collection.date.formatted(date: .abbreviated, time: .omitted))
+                            Spacer()
+                            Text(collection.amount.formatted(.currency(code: currencyCode)))
+                        }
+                    }
                 }
-                HStack {
-                    Text("Collections")
-                    Spacer()
-                    Text(collectionsTotal.formatted(.currency(code: currencyCode)))
-                        .monospaced()
+                
+                Section("Other Income") {
+                    ForEach(otherIncomes) { otherIncome in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(otherIncome.date.formatted(date: .abbreviated, time: .omitted))
+                                Text(otherIncome.info)
+                                    .font(.footnote)
+                            }
+                            Spacer()
+                            Text(otherIncome.amount.formatted(.currency(code: currencyCode)))
+                        }
+                    }
                 }
-                HStack {
-                    Text("Other Income")
-                    Spacer()
-                    Text(otherIncomeTotal.formatted(.currency(code: currencyCode)))
-                        .monospaced()
+                
+                Section("Rent Payments") {
+                    ForEach(rentPayments) { rentPayment in
+                        HStack {
+                            Text(rentPayment.date.formatted(date: .abbreviated, time: .omitted))
+                            Spacer()
+                            Text(rentPayment.amount.formatted(.currency(code: currencyCode)))
+                        }
+                    }
                 }
-                HStack {
-                    Text("Rent")
-                    Spacer()
-                    Text((rentPaymentsTotal * -1).formatted(.currency(code: currencyCode)))
-                        .monospaced()
+                
+                Section("Other Expenses") {
+                    ForEach(otherExpenses) { otherExpense in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(otherExpense.date.formatted(date: .abbreviated, time: .omitted))
+                                Text(otherExpense.info)
+                                    .font(.footnote)
+                            }
+                            Spacer()
+                            Text(otherExpense.amount.formatted(.currency(code: currencyCode)))
+                        }
+                    }
                 }
-                HStack {
-                    Text("Other Expenses")
-                    Spacer()
-                    Text((otherExpensesTotal * -1).formatted(.currency(code: currencyCode)))
-                        .monospaced()
+                
+                Section("Group Conscience") {
+                    ForEach(groupConsciencePayments) { payment in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(payment.date.formatted(date: .abbreviated, time: .omitted))
+                                Text(payment.type)
+                                    .font(.footnote)
+                            }
+                            Spacer()
+                            Text(payment.amount.formatted(.currency(code: currencyCode)))
+                        }
+                    }
                 }
-                HStack {
-                    Text("Prudent Reserve")
-                    Spacer()
-                    Text((meeting.prudentReserve * -1).formatted(.currency(code: currencyCode)))
-                        .monospaced()
-                }
-                HStack {
-                    Text("Group Conscience")
-                    Spacer()
-                    Text((groupConscienceTotal * -1).formatted(.currency(code: currencyCode)))
-                        .monospaced()
-                }
-                HStack {
-                    Text("Ending Balance")
-                    Spacer()
-                    Text(endingBalance.formatted(.currency(code: currencyCode)))
-                        .monospaced()
-                }
+                
             }
+            .formStyle(.grouped)
             .navigationTitle("Report")
         } else {
             ContentUnavailableView("No meeting selected", systemImage: "bubble.left.and.exclamationmark.bubble.right")
